d1181b9f5b8f2efd5e5bd29b0c6b309f13ea7753
marcandy <marcandy.nj@gmail.com>
2016-12-05 17:18:43 -0700

    review

--- arraysPractice.js
+++ arraysPractice.js
@@ -214,7 +214,12 @@ for(var i = 0; i < num2; i++){
 //Write a function called 'longer' that is given arr1 and arr2 as it's only arguments. Return the array which is longest.
 
   //Code Here
-
+function longer(arr1, arr2) {
+  if (arr1.length > arr2.length) {
+    return arr1;
+  }
+  return arr2;
+}
 
 /*As a continuation of the previous problem, write another function called 'both'.
   Your 'both' function will be given two arguments, arr1 and arr2 (from the previous example).
@@ -223,7 +228,17 @@ for(var i = 0; i < num2; i++){
 */
 
   //Code Here
-
+function both(arr1, arr2) {
+  var newB = [];
+  for (var i = 0; i < arr1.length; i++) {
+    for (var j = 0; j < arr2.length; j++) {
+      if(arr1[i] === arr2[j]) {
+        newB.push(arr[i])
+      }
+    }
+  }
+  return newB;
+}
 
 
 
@@ -263,12 +278,17 @@ array with those four objects. After that console.log the length of the Array an
 sure that it's equal to 4. */
 
   //Code Here
-
+  devMountainEmployees.push(tyler, cahlan, ryan, colt);
+  console.log(devMountainEmployees);
 /*Now let's say Cahlan has a mental breakdown and has to take a leave of absence to 'find himself'.
 Loop through your devMountainEmployees until you find cahlan, then remove him from the array.*/
 
   //Code Here
-
+for (var i = 0; i < devMountainEmployees.length; i++) {
+  if ( devMountainEmployees[i] == cahlan) {
+    devMountainEmployees.splice(i, 1);
+  }
+}
 
 
 
